af595e95a742d7e8f041bf38c8dc100af5e23e5e
bala-amypo <bala@amypo.in>
2025-12-17 09:31:50 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StudentServiceimpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentServiceimpl.java
@@ -1,9 +1,9 @@
 package com.example.demo.service.Impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.demo.service.StudentEntity;
+import com.example.demo.service.StudentService;
 import com.example.demo.repository.StudentRepository;
-import com.example.demo.service.StudentRepository
+import com.example.demo.entity.StudentEntity;
 @Service
 public class StudentServiceimpl implements StudentService{
    @Autowired StudentRepository student;//  acces the method in diff files
