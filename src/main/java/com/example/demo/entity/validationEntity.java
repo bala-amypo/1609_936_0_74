358faa6cde6f3cb86e0935af140ef634bbfc4d4e
bala-amypo <bala@amypo.in>
2025-12-18 07:58:01 +0000

    User commit

--- src/main/java/com/example/demo/entity/validationEntity.java
+++ src/main/java/com/example/demo/entity/validationEntity.java
@@ -1,11 +1,61 @@
 package com.example.demo.entity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 @Entity
 public class validationEntity{
-     private Long id;
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @Not
     private String username;
     private String email;
     private String password;
     private Interger age;
+    public void setId(Long id){
+        this.id=id;
+    }
+    public void setUsername(String username){
+        this.username=username;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+     public void setPassword(String password){
+        this.password=password;
+    }
+    public void setAge(Integer Age){
+        this.createdAt=createdAt;
+    }
+    public Long getId(){
+        return id;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
+   
+    public Age Age(){
+        return createdAt;
+    }
+    public StudentEntity(Integer id,String username,String email,String password,Interger Age){
+        this.username=username;
+        this.email=email;
+        this.password=password;
+        this.createdAt=createdAt;
+    }
+    public StudentEntity(){
+
+    }
+
+
+
+}
     
 }
