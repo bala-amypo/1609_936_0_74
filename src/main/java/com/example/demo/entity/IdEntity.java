074955cab6549985820f43a6c5a524de88c998e7
bala-amypo <bala@amypo.in>
2025-12-19 10:01:14 +0000

    User commit

--- src/main/java/com/example/demo/entity/IdEntity.java
+++ src/main/java/com/example/demo/entity/IdEntity.java
@@ -1,33 +1,31 @@
 package com.example.demo.entity;
-import java.util.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.JoinColum;
-import jakarta.persistence.OneToOne;
-// import lombok.Setter;
-// import lombok.Getter;
+
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-// @Setter
-// @Getter
-// @Entity
-public class StudentEntity{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Integer id;
-    private int cardno;
-    private String email;
-    //private int student_id;
-    @OneToOne
-    @JoinColumn(name="student_id")
-    private StudentId student;
-   
+public class StudentEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private int cardno;
+
+    private String email;
+
+    @OneToOne
+    @JoinColumn(name = "student_id")
+    private StudentId student;
 }
