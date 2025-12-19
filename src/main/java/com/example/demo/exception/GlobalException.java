d9ee191a8205a1011646da333338324e1d532b46
bala-amypo <bala@amypo.in>
2025-12-19 06:03:31 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalException.java
+++ src/main/java/com/example/demo/exception/GlobalException.java
@@ -1,14 +1,23 @@
-package com.example.demo.exception;
-import org.springbootframework.web.bind.annotation.RestControllerAdvice;
-import org.springbootframework.web.bind.annotation.ExceptionHandler;
-import org.springbootframework.web.bind.annotation.ResponseEntity;
-
-
-@RestControllerAdvice
-public class GlobalException{
-    @ExceptionHandler(validationException.class)
-    public ResponseEntity<String>handlerValidation(validationException ex){
-        return new Response<String>(ex.getMessage(),HttpStatus.BAD_GATEWAY);
-
-    }
-}
+[ERROR] Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:3.13.0:compile (default-compile) on project demo: Compilation failure: Compilation failure: 
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/exception/GlobalException.java:[2,51] package org.springbootframework.web.bind.annotation does not exist
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/exception/GlobalException.java:[3,51] package org.springbootframework.web.bind.annotation does not exist
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/exception/GlobalException.java:[4,51] package org.springbootframework.web.bind.annotation does not exist
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/exception/GlobalException.java:[7,2] cannot find symbol
+[ERROR]   symbol: class RestControllerAdvice
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/exception/GlobalException.java:[10,12] cannot find symbol
+[ERROR]   symbol:   class ResponseEntity
+[ERROR]   location: class com.example.demo.exception.GlobalException
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/exception/GlobalException.java:[9,6] cannot find symbol
+[ERROR]   symbol:   class ExceptionHandler
+[ERROR]   location: class com.example.demo.exception.GlobalException
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/exception/GlobalException.java:[11,20] cannot find symbol
+[ERROR]   symbol:   class ResponseEntity
+[ERROR]   location: class com.example.demo.exception.GlobalException
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/exception/GlobalException.java:[11,59] cannot find symbol
+[ERROR]   symbol:   variable HttpStatus
+[ERROR]   location: class com.example.demo.exception.GlobalException
+[ERROR] -> [Help 1]
+[ERROR] 
+[ERROR] To see the full stack trace of the errors, re-run Maven with the -e switch.
+[ERROR] Re-run Maven using the -X switch to enable full debug logging.
+[ERROR] 
